e5b872357c9b4d0438bddf4bceca698d2bf5bd8c
jtsang02 <jtsang02@student.ubc.ca>
2021-11-09 09:53:44 -0800

    PA bonuses

--- PA14_w9/PA14_w9/Program.cs
+++ PA14_w9/PA14_w9/Program.cs
@@ -3,18 +3,19 @@
 public class Program {
     static void Main () {
 
+        // test Swap function
         int x = 1;
         int y = 2;
         Swap(ref x, ref y);
 
         Console.WriteLine($"x is now {x} and y is now {y}");
 
+        // test GetInt function
         if (GetInt(out int num))
             Console.WriteLine($"The entered number is {num}");
-        
         else
             Console.WriteLine("not a valid integer.");
-        
+
     }
 
     /// <summary>
@@ -23,9 +24,11 @@ public class Program {
     /// <param name="x">first int parameter</param>
     /// <param name="y">second int parameter</param>
     public static void Swap (ref int x, ref int y) {
+
         int temp = x;
         x = y;
         y = temp;
+
     }
 
     /// <summary>
@@ -35,9 +38,11 @@ public class Program {
     /// <param name="userInput">an out parameter to output the int value</param>
     /// <returns>true if an int is successfully received from the console, false otherwise</returns>
     public static bool GetInt (out int userInput) {
+
         Console.Write("enter an integer: ");
         string s = Console.ReadLine().Trim();
         bool isInt = int.TryParse(s, out userInput);
         return isInt;
+
     }
 }
---
+++ PA_Bonus2_W4/PA_Bonus2_W4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program {
+
+    // Return the length of longest balanced parentheses string.
+    static int balancedLen (string S) {
+
+        int longestCount = 0;
+        Stack<char> balancedStack = new Stack<char>();
+
+        for (var i = 0; i < S.Length; i++) {
+
+            if (S[i] == '(')
+                balancedStack.Push(S[i]);
+            else if (S[i] == ')')
+                balancedStack.Pop();
+
+            if (balancedStack.Count == 0)
+                longestCount = i + 1;
+        }
+        return longestCount;
+    }
+
+    public static void Main () {
+
+        string str = "(((()())()))((";
+        Console.WriteLine(balancedLen(str));
+
+        string S = "((()))((()()()";
+        Console.WriteLine(balancedLen(S));
+    }
+}
