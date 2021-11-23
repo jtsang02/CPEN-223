9c8c1484c92038d761b57fcf75397e66696d3642
jtsang02 <jtsang02@student.ubc.ca>
2021-11-22 17:28:33 -0800

    PA18 & PA19

--- PA16_w11/PA16_w11/Program.cs
+++ PA16_w11/PA16_w11/Program.cs
@@ -4,23 +4,10 @@ public class MyClass {
     static void Main () {   //To implement: used for testing
 
         //include two test cases for each of the methods
-        long[] interateArray = new long[20];
-        long[] recursionArray = new long[20];
-
         for (int n = 0; n < 20; n++) {
-            interateArray[n] = Factorial_iteration(n);
-            recursionArray[n] = Factorial_recursion(n);
+            Console.Write($"Recursive: {n}!={Factorial_recursion(n)}   ");
+            Console.WriteLine($"Iterative: {n}!= {Factorial_iteration(n)}");
         }
-
-        Console.WriteLine("The factorials from 0 to 20 by iteration are:");
-        foreach (long val in interateArray)
-            Console.Write($"{val} ");
-        
-        Console.WriteLine();
-
-        Console.WriteLine("The factorials from 0 to 20 by recursion are:");
-        foreach (long val in recursionArray)
-            Console.Write($"{val} ");
     }
 
     public static long Factorial_iteration (int num) {  // implement factorial using iteration (non-recursively)
---
+++ PA18_w12/PA18_w12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PA18_w12 {
+    public class PA {
+        static void Main () {
+            
+            Console.WriteLine(RecursiveSum(5));
+            Console.WriteLine(StringToInt("5677"));
+        }
+
+        public static int RecursiveSum (int n) {
+
+            if (n == 0)
+                return 0;
+
+            int sum;
+            sum = n + RecursiveSum(n - 1);
+            return sum;
+
+        }
+        public static int StringToInt (string str) {
+
+            if (str.Length == 1)
+                return str[0] - '0';
+
+            return (int)(StringToInt(str.Substring(1)) + ((str[0] - '0') * Math.Pow(10, str.Length - 1)));
+        }
+    }
+}
